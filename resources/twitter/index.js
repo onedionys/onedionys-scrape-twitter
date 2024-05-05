c661964a936e7c7dc188ae41c720a0f6a3c43311
One Dionys <161098412+onedionys@users.noreply.github.com>
2024-05-05 22:30:17 +0700

    chore: bring back

--- resources/twitter/index.js
+++ resources/twitter/index.js
@@ -1,60 +1,13 @@
-const { referrerPolicy } = require('helmet');
 const fetch = require('node-fetch');
 
-async function getHome(cookie) {
-    const url = "https://sepolia.basescan.org/token/0x87c51cd469a0e1e2af0e0e597fd88d9ae4baa967";
-
-    const options = {
-        method: "GET",
-        body: null,
-        headers: {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9,id;q=0.8",
-            "cache-control": "no-cache",
-            "pragma": "no-cache",
-            "priority": "u=0, i",
-            "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "cookie": cookie
-        },
-        referrerPolicy: "strict-origin-when-cross-origin"
-    }
-
-    try {
-        const response = await fetch(url, options);
-        const html = await response.text();
-
-        return {
-            status: true,
-            data: html,
-            error: null
-        }
-    } catch(error) {
-        return {
-            status: false,
-            data: null,
-            error: error
-        }
-    }
-}
-
-async function getTwitter(req, res) {
-    try {
-        let cookie = "_ga_TWEL8GRQ12=GS1.1.1712807855.6.0.1712807868.0.0.0; _gid=GA1.2.1823047506.1714660330; ASP.NET_SessionId=bdfv4uwhtxbs501s0y40zfhv; _ga_N2ZMSQVFZ8=GS1.1.1714913762.106.0.1714913762.0.0.0; _ga=GA1.2.1108818328.1709989960; _gat_gtag_UA_46998878_38=1";
-        let fetchUser = await getHome(cookie);
-
-        res.status(200).send(fetchUser.data);
-    } catch (error) {
-        res.status(422).json({
-            data: null
-        });
-    }
+function getTwitter(req, res) {
+    res.json({
+        status: 1,
+        status_code: 200,
+        message: "API ini digunakan untuk mengambil daftar komentar twitter berdasarkan kata kunci.",
+        info_error: null,
+        data: null
+    })
 }
 
 module.exports = { getTwitter };
